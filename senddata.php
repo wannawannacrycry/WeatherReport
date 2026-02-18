4b45c3cf8853ed15b6dc71ed135807338b8da9b7
wannawannacrycry <157525206+wannawannacrycry@users.noreply.github.com>
2026-02-18 09:54:32 -0600

    Prepared SQL statements amd updated code for consistency

--- senddata.php
+++ senddata.php
@@ -1,12 +1,26 @@
 <?php
-$host = 'localhost';
-$dbname = 'weatherdb';
-$username = 'root';
-$password = 'root';
+$host = "localhost";
+$dbname = "weatherdb";
+$username = "root";
+$password = "root";
 
-$data = json_decode(file_get_contents('php://input'), true);
+$data = json_decode(file_get_contents("php://input"), true);
 
     $connection = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
 
+    $sql = "INSERT INTO weather_history 
+            (city_name, state_name, temperature, description, wind_speed) 
+            VALUES (:city, :state, :temperature, :description, :wind_speed)";
 
+    $statement = $connection->prepare($sql);
+
+    $statement->bindParam(":city", $data["city"]);
+    $statement->bindParam(":state", $data["state"]);
+    $statement->bindParam(":temperature", $data["temperature"]);
+    $statement->bindParam(":description", $data["description"]);
+    $statement->bindParam(":wind_speed", $data["wind_speed"]);
+
+    $statement->execute();
+
+    echo json_encode(["success" => true, "message" => "Data successfully sent to DB"]);
 ?>
