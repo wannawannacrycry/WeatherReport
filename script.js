11ddacea29f86a2d77f1fbf0397e939841af9d12
wannawannacrycry <157525206+wannawannacrycry@users.noreply.github.com>
2026-02-18 07:57:05 -0600

    Committing already existing files

---
+++ script.js
@@ -0,0 +1,29 @@
+const url = 'https://api.openweathermap.org/data/2.5/weather'; // openweathermap base url
+const apiKey = 'XXXXXXXXXXXXXXXXXX'; // API Key
+
+async function verifyData(cityName, stateID) // Verify that the provided input has a valid output
+{
+    const inputData = `${url}?q=${cityName},${stateID}&appid=${apiKey}&units=imperial`; //Out together full API request
+    try {
+        const res = await fetch(inputData); // Try to fetch data given the input
+        const data = await res.json(); // Return promise
+        if (res.ok) {
+            showWeatherData(data);
+        } 
+        else {
+            alert(`${cityName} not found in ${stateID}. Please try again.`); // Alert the user if the city can't be found 
+        }
+	} 
+    catch (error) {
+		console.error('Error fetching weather data:', error); // Miscelaneous error catch
+	}
+}
+
+function showWeatherData(data) // Assign vars for use with HTML
+{
+    $('#city-name').text(data.name); // City Name
+    $('#date').text(moment().format('MMMM Do YYYY, h:mm:ss a')); // Date (Month, Day, Year, Time)
+    $('#temperature').html(`${Math.round(data.main.temp)}°F`); // Temperature
+    $('#description').text(data.weather[0].description); // Weather description
+    $('#wind-speed').html(`Wind Speed: ${data.wind.speed} mph`); // Wind speed
+}
