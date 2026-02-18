43f6380c00ac579e036494b69e918129b37a56e9
wannawannacrycry <157525206+wannawannacrycry@users.noreply.github.com>
2026-02-18 11:24:22 -0600

    Basic DB connection

--- reporthistory.php
+++ reporthistory.php
@@ -14,7 +14,22 @@
             </nav>
         </header>
         <h1>View Report History</h3>
+        <?php
+            $host = "localhost";
+            $dbname = "weatherdb";
+            $username = "root";
+            $password = "root";
+            try{
+                $connection = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
 
+            }
+            catch(PDOException $exception){
+                echo "Error connecting and pulling from DB: " . $exception->getMessage();
+            }
+
+
+
+        ?>
     </body>
 
 
