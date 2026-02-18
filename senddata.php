88a54f95078218205934cd48302086a46baa3544
wannawannacrycry <157525206+wannawannacrycry@users.noreply.github.com>
2026-02-18 09:57:22 -0600

    Added try and catch

--- senddata.php
+++ senddata.php
@@ -5,22 +5,28 @@ $username = "root";
 $password = "root";
 
 $data = json_decode(file_get_contents("php://input"), true);
+    try{
+        $connection = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
 
-    $connection = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+        $sql = "INSERT INTO weather_history 
+                (city_name, state_name, temperature, description, wind_speed) 
+                VALUES (:city, :state, :temperature, :description, :wind_speed)";
 
-    $sql = "INSERT INTO weather_history 
-            (city_name, state_name, temperature, description, wind_speed) 
-            VALUES (:city, :state, :temperature, :description, :wind_speed)";
+        $statement = $connection->prepare($sql);
 
-    $statement = $connection->prepare($sql);
+        $statement->bindParam(":city", $data["city"]);
+        $statement->bindParam(":state", $data["state"]);
+        $statement->bindParam(":temperature", $data["temperature"]);
+        $statement->bindParam(":description", $data["description"]);
+        $statement->bindParam(":wind_speed", $data["wind_speed"]);
 
-    $statement->bindParam(":city", $data["city"]);
-    $statement->bindParam(":state", $data["state"]);
-    $statement->bindParam(":temperature", $data["temperature"]);
-    $statement->bindParam(":description", $data["description"]);
-    $statement->bindParam(":wind_speed", $data["wind_speed"]);
+        $statement->execute();
 
-    $statement->execute();
-
-    echo json_encode(["success" => true, "message" => "Data successfully sent to DB"]);
+        echo json_encode(["success" => true, "message" => "Data successfully sent to DB"]);
+    }
+    catch(PDOException $exception)
+    {
+        echo json_encode(["success" => false, "message" => "Error sending data to DB: ". $exception->getMessage()]);
+    }
+    
 ?>
