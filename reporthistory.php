34267c6c1c60a96b451692bf0957171cac7cd329
wannawannacrycry <157525206+wannawannacrycry@users.noreply.github.com>
2026-02-18 11:08:33 -0600

    Gave reporthistory.php styling, corrected naming

---
+++ reporthistory.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Weather History</title>
+        <link rel="stylesheet" href="style.css">
+    </head>
+    <body>
+        <header>
+            <nav>
+                <div class="navbar">
+                    <a href="index.html">Generate Report</a>
+                    <a href="history.php">View Report History</a>
+                </div>
+            </nav>
+        </header>
+    </body>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+</html>
