a0842e3999a43b9cfd48b5d00963fcd21714cbe8
wannawannacrycry <157525206+wannawannacrycry@users.noreply.github.com>
2026-02-18 09:49:26 -0600

    Get data from js and establish connection with database

--- senddata.php
+++ senddata.php
@@ -4,5 +4,9 @@ $dbname = 'weatherdb';
 $username = 'root';
 $password = 'root';
 
+$data = json_decode(file_get_contents('php://input'), true);
+
+    $connection = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+
 
 ?>
